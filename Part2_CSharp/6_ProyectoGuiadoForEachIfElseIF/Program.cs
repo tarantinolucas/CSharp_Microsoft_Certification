44e5edc6652a36ef84eaf01916e32a67856aa510
Lucas Tarantino <tarantinolucas@hotmail.com>
2025-07-22 07:00:12 -0300

    Avance en proyecto, reestructuración de código.

--- Part2_CSharp/6_ProyectoGuiadoForEachIfElseIF/Program.cs
+++ Part2_CSharp/6_ProyectoGuiadoForEachIfElseIF/Program.cs
@@ -1,6 +1,6 @@
-﻿using System;
+﻿/* Codigo base
 
-// initialize variables - graded assignments 
+// initialize variables - graded assignments
 int currentAssignments = 5;
 
 int sophia1 = 90;
@@ -52,6 +52,34 @@ Console.WriteLine("Sophia:\t\t" + sophiaScore + "\tA-");
 Console.WriteLine("Andrew:\t\t" + andrewScore + "\tB+");
 Console.WriteLine("Emma:\t\t" + emmaScore + "\tB");
 Console.WriteLine("Logan:\t\t" + loganScore + "\tA-");
+
+Console.WriteLine("Press the Enter key to continue");
+Console.ReadLine();
+
+*/
+
+// En base al codigo anterior, actualizaremos el mismo para que se utilicen bucles foreach, instrucciones de decision como if, else if y else, y se elimine el uso de variables innecesarias.
+
+int currentAssignments = 5;
+
+int[] sophiaScores = new int[] { 90, 86, 87, 98, 100 };
+int[] andrewScores = new int[] { 92, 89, 81, 96, 90 };
+int[] emmaScores = new int[] { 90, 85, 87, 98, 68 };
+int[] loganScores = new int[] { 90, 95, 87, 88, 96 };
+
+int sophiaSum = 0;
+
+decimal sophiaScore;
+
+foreach (int score in sophiaScores)
+{
+    sophiaSum += score;
+}
+
+sophiaScore = (decimal)sophiaSum / currentAssignments;
+
+Console.WriteLine("Student\t\tGrade\n");
+Console.WriteLine("Sophia:\t\t" + sophiaScore + "\tA-");
 
 Console.WriteLine("Press the Enter key to continue");
 Console.ReadLine();
