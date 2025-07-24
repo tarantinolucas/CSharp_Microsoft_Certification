f23c35c42895d0e10181acf47dc99317de738c0c
Lucas Tarantino <tarantinolucas@hotmail.com>
2025-07-24 20:32:39 -0300

    Preparación de los archivos.

--- Part3_CSharp/2_ControlDeLogicaYAmbitoDeVariables/Program.cs
+++ Part3_CSharp/2_ControlDeLogicaYAmbitoDeVariables/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+Console.WriteLine("-----------------------------------------");
+Console.WriteLine("Parte 3: Introducción a C#");
+Console.WriteLine("-----------------------------------------");
+Console.WriteLine("2_Control de Lógica y Ámbito de Variables");
+Console.WriteLine("-----------------------------------------\n");
+
+// -------------------------------------------------
+// Unidad 2_Bloques de código y ambito de variables
+// -------------------------------------------------
+Console.WriteLine("--------------------------------------------------");
+Console.WriteLine("Unidad 2: Bloques de código y ámbito de variables");
+Console.WriteLine("--------------------------------------------------\n");
+
+// -------------------------------------------------
+// Fin del programa
+// -------------------------------------------------
+Console.ReadKey();
+Console.WriteLine("Pulse alguna tecla para continuar...");
