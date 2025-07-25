dc05968b807cac1a2c26a0ba5c6cc31400d9ffdc
Lucas Tarantino <tarantinolucas@hotmail.com>
2025-07-25 11:16:39 -0300

    Se comienza nueva unidad: 3_Ramificacion con Switch case.

---
+++ Part3_CSharp/3_RamificacionConSwitchCase/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Reflection.Metadata;
+
+Console.Clear();
+
+Console.WriteLine("-----------------------------------------");
+Console.WriteLine("Parte 3: Introducción a C#");
+Console.WriteLine("-----------------------------------------");
+Console.WriteLine("3_Ramificación con Switch Case");
+Console.WriteLine("-----------------------------------------\n");
+
+// -------------------------------------------------
+// Unidad 2_Implementacion de una instruccion switch
+// -------------------------------------------------
+Console.WriteLine("--------------------------------------------------");
+Console.WriteLine("Unidad 2: Implementacion de una instruccion switch");
+Console.WriteLine("--------------------------------------------------\n");
+
+// Probando una instrucción switch
+int employeeLevel = 200;
+string employeeName = "John Smith";
+
+string title = "";
+
+switch (employeeLevel)
+{
+    case 100:
+        title = "Junior Associate";
+        break;
+    case 200:
+        title = "Senior Associate";
+        break;
+    case 300:
+        title = "Manager";
+        break;
+    case 400:
+        title = "Senior Manager";
+        break;
+    default:
+        title = "Associate";
+        break;
+}
+
+// -------------------------------------------------
+// Fin del programa
+// -------------------------------------------------
+Console.WriteLine("\n\nPulse alguna tecla para continuar...");
+Console.ReadKey();
