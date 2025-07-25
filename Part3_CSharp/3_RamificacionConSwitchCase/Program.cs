635d1063d2adc85f141d70454d1d0b7151761e7b
Lucas Tarantino <tarantinolucas@hotmail.com>
2025-07-25 17:37:03 -0300

    Se finaliza módulo 3_Ramificación con Switch Case.

--- Part3_CSharp/3_RamificacionConSwitchCase/Program.cs
+++ Part3_CSharp/3_RamificacionConSwitchCase/Program.cs
@@ -16,7 +16,7 @@ Console.WriteLine("Unidad 2: Implementacion de una instruccion switch");
 Console.WriteLine("--------------------------------------------------\n");
 
 // Probando una instrucción switch
-int employeeLevel = 200;
+int employeeLevel = 100;
 string employeeName = "John Smith";
 
 string title = "";
@@ -24,11 +24,12 @@ string title = "";
 switch (employeeLevel)
 {
     case 100:
-        title = "Junior Associate";
-        break;
     case 200:
         title = "Senior Associate";
         break;
+    //case 200:
+    //title = "Senior Associate";
+    //break;
     case 300:
         title = "Manager";
         break;
@@ -39,6 +40,130 @@ switch (employeeLevel)
         title = "Associate";
         break;
 }
+
+Console.WriteLine($"{employeeName}, {title}");
+
+// -------------------------------------------------
+// Unidad 3_Conversion a instruccion switch
+// -------------------------------------------------
+Console.WriteLine("--------------------------------------------------");
+Console.WriteLine("Unidad 3: Conversion a instruccion switch");
+Console.WriteLine("--------------------------------------------------\n");
+
+// Desafío de código: reescribir if-elseif-else mediante una instrucción switch
+
+/* Codigo original
+
+// SKU = Stock Keeping Unit.
+// SKU value format: <product #>-<2-letter color code>-<size code>
+string sku = "01-MN-L";
+
+string[] product = sku.Split('-');
+
+string type = "";
+string color = "";
+string size = "";
+
+if (product[0] == "01")
+{
+    type = "Sweat shirt";
+} else if (product[0] == "02")
+{
+    type = "T-Shirt";
+} else if (product[0] == "03")
+{
+    type = "Sweat pants";
+}
+else
+{
+    type = "Other";
+}
+
+if (product[1] == "BL")
+{
+    color = "Black";
+} else if (product[1] == "MN")
+{
+    color = "Maroon";
+} else
+{
+    color = "White";
+}
+
+if (product[2] == "S")
+{
+    size = "Small";
+} else if (product[2] == "M")
+{
+    size = "Medium";
+} else if (product[2] == "L")
+{
+    size = "Large";
+} else
+{
+    size = "One Size Fits All";
+}
+
+Console.WriteLine($"Product: {size} {color} {type}");
+
+*/
+
+// SKU = Stock Keeping Unit.
+// SKU value format: <product #>-<2-letter color code>-<size code>
+string sku = "01-MN-L";
+
+string[] product = sku.Split('-');
+
+string type = "";
+string color = "";
+string size = "";
+
+switch (product[0])
+{
+    case "01":
+        type = "Sweat shirt";
+        break;
+    case "02":
+        type = "T-Shirt";
+        break;
+    case "03":
+        type = "Sweat pants";
+        break;
+    default:
+        type = "Other";
+        break;
+}
+
+switch (product[1])
+{
+    case "BL":
+        color = "Black";
+        break;
+    case "MN":
+        color = "Maroon";
+        break;
+    default:
+        color = "White";
+        break;
+}
+
+switch (product[2])
+{
+    case "S":
+        size = "Small";
+        break;
+    case "M":
+        size = "Medium";
+        break;
+    case "L":
+        size = "Large";
+        break;
+    default:
+        size = "One Size Fits All";
+        break;
+}
+
+Console.WriteLine($"Product: {size} {color} {type}");
 
 // -------------------------------------------------
 // Fin del programa
