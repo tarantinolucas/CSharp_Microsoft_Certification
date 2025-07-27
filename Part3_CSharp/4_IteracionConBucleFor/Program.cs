8a3df51bdb9cc4c8323509ac57fecd02219db198
Lucas Tarantino <tarantinolucas@hotmail.com>
2025-07-27 16:05:10 -0300

    Se finaliza modulo 4_Iteracion con bucle for.

--- Part3_CSharp/4_IteracionConBucleFor/Program.cs
+++ Part3_CSharp/4_IteracionConBucleFor/Program.cs
@@ -82,6 +82,43 @@ foreach (var name in names3)
     Console.WriteLine(name);
 }
 
+// Unidad 3: Realización de una actividad desafio con instrucciones for e if
+Console.WriteLine("\n--------------------------------------------------");
+Console.WriteLine("Unidad 3: Desafío FizzBuzz con instrucciones for e if");
+Console.WriteLine("--------------------------------------------------\n");
+
+/*
+Desafío FizzBuzz:
+
+Desafío de código: Implementación de las reglas de desafío de FizzBuzz
+Estas son las reglas de FizzBuzz que necesita implementar en el proyecto de código:
+
+Valores de salida de 1 a 100, un número por línea, dentro del bloque de código de una instrucción de iteración.
+Si el valor actual es divisible por 3, se imprime el término Fizz junto al número.
+Si el valor actual es divisible por 5, se imprime el término Buzz junto al número.
+Si el valor actual es divisible por 3 y por 5, se imprime el término FizzBuzz junto al número.
+*/
+
+for (int i = 1; i <= 100; i++)
+{
+    if (i % 3 == 0 && i % 5 == 0)
+    {
+        Console.WriteLine($"{i} - FizzBuzz");
+    }
+    else if (i % 3 == 0)
+    {
+        Console.WriteLine($"{i} - Fizz");
+    }
+    else if (i % 5 == 0)
+    {
+        Console.WriteLine($"{i} - Buzz");
+    }
+    else
+    {
+        Console.WriteLine($"{i}");
+    }
+}
+
 // -------------------------------------------------
 // Fin del programa
 // -------------------------------------------------
