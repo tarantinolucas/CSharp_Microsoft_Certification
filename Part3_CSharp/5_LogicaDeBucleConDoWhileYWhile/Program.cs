fff3d1d05d7d619f4bdda501f200500bd0c9b941
Lucas Tarantino <tarantinolucas@hotmail.com>
2025-07-27 16:10:09 -0300

    Se comienza con nuevo Módulo: 5_Adición de lógica de bucle al código mediante las instrucciones do while y while.

---
+++ Part3_CSharp/5_LogicaDeBucleConDoWhileYWhile/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection.Metadata;
+
+Console.Clear();
+
+Console.WriteLine("-----------------------------------------");
+Console.WriteLine("Parte 3: Introducción a C#");
+Console.WriteLine("-----------------------------------------");
+Console.WriteLine("5_Lógica de Bucle con Do While y While");
+Console.WriteLine("-----------------------------------------\n");
+
+// -------------------------------------------------
+// Unidad 5_Lógica de Bucle con Do While y While
+// -------------------------------------------------
+Console.WriteLine("--------------------------------------------------");
+Console.WriteLine("Unidad 2: Creación de bucles con Do While y While");
+Console.WriteLine("--------------------------------------------------\n");
+
+// -------------------------------------------------
+// Fin del programa
+// -------------------------------------------------
+Console.WriteLine("\n\nPulse alguna tecla para continuar...");
+Console.ReadKey();
