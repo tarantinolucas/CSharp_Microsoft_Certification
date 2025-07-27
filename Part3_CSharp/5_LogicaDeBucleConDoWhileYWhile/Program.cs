7830cd552a89956074ac9c7b42559130e1e19e15
Lucas Tarantino <tarantinolucas@hotmail.com>
2025-07-27 16:46:14 -0300

    Se finalizo la unidad 2 del modulo 5: Creación de bucles con Do While y While.

--- Part3_CSharp/5_LogicaDeBucleConDoWhileYWhile/Program.cs
+++ Part3_CSharp/5_LogicaDeBucleConDoWhileYWhile/Program.cs
@@ -15,6 +15,62 @@ Console.WriteLine("--------------------------------------------------");
 Console.WriteLine("Unidad 2: Creación de bucles con Do While y While");
 Console.WriteLine("--------------------------------------------------\n");
 
+/*
+Vamos a escribir código que seguirá generando números aleatorios entre 1 y 10 hasta que generemos el número 7. Podría tomar solo una iteración para obtener un 7, o podría tomar docenas de iteraciones.
+*/
+
+Random random = new();
+int current = 0;
+int counter = 0;
+
+do
+{
+    current = random.Next(1, 11);
+    Console.WriteLine($"Número generado: {current}");
+    counter++;
+} while (current != 7);
+Console.WriteLine($"\n-----------------------------------------------------");
+Console.WriteLine($"¡Número 7 generado después de {counter} iteraciones!");
+Console.WriteLine($"-----------------------------------------------------\n");
+
+// Escibimos una instruccion While que itere siempre que un numero aleatorio sea mayor que algun valor.
+
+int current2 = random.Next(1, 11);
+int counter2 = 0;
+
+if (current2 < 3)
+    Console.WriteLine($"Número generado: {current2}");
+
+while (current2 >= 3)
+{
+    Console.WriteLine($"Número generado: {current2}");
+    current2 = random.Next(1, 11);
+    counter2++;
+}
+Console.WriteLine($"\n-----------------------------------------------------");
+Console.WriteLine($"¡Números generados mayores o iguales que 3: {counter2}!");
+Console.WriteLine($"-----------------------------------------------------\n");
+
+// Usando una instruccion continue para ir directamente a la expresión booleana
+int current3 = random.Next(1, 11);
+int counter3 = 0;
+
+do
+{
+    counter3++;
+    current3 = random.Next(1, 11);
+    if (current3 >= 8)
+    {
+        Console.WriteLine($"Número generado: {current3} // Se salta la iteración.");
+        continue;
+    }
+    Console.WriteLine($"Número generado: {current3}");
+} while (current3 != 7);
+
+Console.WriteLine($"\n-----------------------------------------------------");
+Console.WriteLine($"¡Número 7 generado después de {counter3} iteraciones!");
+Console.WriteLine($"-----------------------------------------------------\n");
+
 // -------------------------------------------------
 // Fin del programa
 // -------------------------------------------------
