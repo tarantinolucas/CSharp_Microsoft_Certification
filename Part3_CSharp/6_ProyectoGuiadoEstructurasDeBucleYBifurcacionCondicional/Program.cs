d8f241ebce61ccdffc150af53ac2e6f7f84e3da0
Lucas Tarantino <tarantinolucas@hotmail.com>
2025-07-28 10:33:50 -0300

    Creación de nuevo proyecto 6_Proyecto guiado: estructuras de bucle y bifurcación condicional.

---
+++ Part3_CSharp/6_ProyectoGuiadoEstructurasDeBucleYBifurcacionCondicional/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection.Metadata;
+
+Console.Clear();
+
+Console.WriteLine("------------------------------------------------------------");
+Console.WriteLine("Parte 3: Introducción a C#");
+Console.WriteLine("----------------------------------------------------------------------");
+Console.WriteLine("6_Proyecto Guiado: Estructuras de Bucle y Bifurcación Condicional");
+Console.WriteLine("----------------------------------------------------------------------");
+
+// -------------------------------------------------
+// Unidad 2_Lógica de Bucle con Do While y While
+// -------------------------------------------------
+Console.WriteLine("--------------------------------------------------");
+Console.WriteLine("Unidad 2: ");
+Console.WriteLine("--------------------------------------------------\n");
+
+// -------------------------------------------------
+// Fin del programa
+// -------------------------------------------------
+Console.WriteLine("\n\nPulse alguna tecla para continuar...");
+Console.ReadKey();
