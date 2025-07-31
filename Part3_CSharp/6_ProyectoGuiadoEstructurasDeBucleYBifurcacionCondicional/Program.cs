efc212b86b812bef2b69d61a8d58051e2bf1108f
Lucas Tarantino <tarantinolucas@hotmail.com>
2025-07-31 07:16:42 -0300

    Módulo finalizado: 6_Proyecto guiado: estructuras de bucle y bifurcación condicional.

--- Part3_CSharp/6_ProyectoGuiadoEstructurasDeBucleYBifurcacionCondicional/Program.cs
+++ Part3_CSharp/6_ProyectoGuiadoEstructurasDeBucleYBifurcacionCondicional/Program.cs
@@ -171,6 +171,110 @@ do
 
             while (anotherPet == "y" && petCount < maxPets)
             {
+                bool validEntry = false;
+
+                do
+                {
+                    Console.WriteLine("\n\rEnter 'dog' or 'cat' to begin a new entry");
+                    readResult = Console.ReadLine();
+                    if (readResult != null)
+                    {
+                        animalSpecies = readResult.ToLower();
+                        if (animalSpecies == "dog" || animalSpecies == "cat")
+                        {
+                            validEntry = false;
+                        }
+                        else
+                        {
+                            validEntry = true;
+                        }
+                    }
+
+                    // build the animal ID number
+                    animalID = animalSpecies.Substring(0, 1) + (petCount + 1).ToString();
+
+                    // get the pet's age. can be ? at initial entry
+                    do
+                    {
+                        int petAge;
+                        Console.WriteLine("Enter the pet's age or enter ? if unknown");
+                        readResult = Console.ReadLine();
+
+                        if (readResult != null)
+                        {
+                            animalAge = readResult;
+                            if (animalAge != "?")
+                            {
+                                validEntry = int.TryParse(animalAge, out petAge);
+                            }
+                            else
+                            {
+                                validEntry = true;
+                            }
+                        }
+                    } while (validEntry == false);
+
+                    // get a description of the pet's physical appearance/condition
+                    do
+                    {
+                        Console.WriteLine(
+                            "Enter a physical description of the pet (size, color, gender, weight, housebroker)"
+                        );
+                        readResult = Console.ReadLine();
+
+                        if (readResult != null)
+                        {
+                            animalPhysicalDescription = readResult.ToLower();
+                            if (animalPhysicalDescription == "")
+                            {
+                                animalPhysicalDescription = "tbd";
+                            }
+                        }
+                    } while (animalPhysicalDescription == "");
+
+                    // get a description of the pet's personality
+                    do
+                    {
+                        Console.WriteLine(
+                            "Enter a description of the pet's personality (likes or dislikes, tricks, energy level)"
+                        );
+                        readResult = Console.ReadLine();
+
+                        if (readResult != null)
+                        {
+                            animalPersonalityDescription = readResult.ToLower();
+                            if (animalPersonalityDescription == "")
+                            {
+                                animalPersonalityDescription = "tbd";
+                            }
+                        }
+                    } while (animalPersonalityDescription == "");
+
+                    // get the pet's nickname. animalNickname can be blank
+                    do
+                    {
+                        Console.WriteLine("Enter a nickname for the pet");
+                        readResult = Console.ReadLine();
+
+                        if (readResult != null)
+                        {
+                            animalNickname = readResult.ToLower();
+                            if (animalNickname == "")
+                            {
+                                animalNickname = "tbd";
+                            }
+                        }
+                    } while (animalNickname == "");
+
+                    // store the pet information in the ourAnimals array (zero based)
+                    ourAnimals[petCount, 0] = "ID #: " + animalID;
+                    ourAnimals[petCount, 1] = "Species: " + animalSpecies;
+                    ourAnimals[petCount, 2] = "Age: " + animalAge;
+                    ourAnimals[petCount, 3] = "Nickname: " + animalNickname;
+                    ourAnimals[petCount, 4] = "Physical description: " + animalPhysicalDescription;
+                    ourAnimals[petCount, 5] = "Personality: " + animalPersonalityDescription;
+                } while (validEntry == false);
+
                 petCount += 1;
 
                 if (petCount < maxPets)
