fdd429fec3aafc422b668c32f29ccb5983dd47ef
tarantinolucas <tarantinolucas@hotmail.com>
2025-08-02 17:25:47 -0300

    Se empezo nuevo módulo: Parte 4_Introducción a C#.

---
+++ Part4_CSharp/1_EleccionDelTipoDeDatoCorrecto/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection.Metadata;
+
+Console.Clear();
+
+Console.WriteLine("-----------------------------------------");
+Console.WriteLine("Parte 3: Introducción a C#");
+Console.WriteLine("-----------------------------------------");
+Console.WriteLine("1_Elección del Tipo de Dato Correcto");
+Console.WriteLine("-----------------------------------------\n");
+
+// -------------------------------------------------
+// Fin del programa
+// -------------------------------------------------
+Console.WriteLine("\n\nPulse alguna tecla para continuar...");
+Console.ReadKey();
