92459934ceb229a5abfd5fc17cd75078753c9970
Lucas Tarantino <tarantinolucas@hotmail.com>
2025-08-04 07:03:22 -0300

    Se realizo la unidad 3: Deteccion de tipos de enteros.

--- Part4_CSharp/1_EleccionDelTipoDeDatoCorrecto/Program.cs
+++ Part4_CSharp/1_EleccionDelTipoDeDatoCorrecto/Program.cs
@@ -3,10 +3,26 @@
 Console.Clear();
 
 Console.WriteLine("-----------------------------------------");
-Console.WriteLine("Parte 3: Introducción a C#");
+Console.WriteLine("Parte 4: Introducción a C#");
 Console.WriteLine("-----------------------------------------");
 Console.WriteLine("1_Elección del Tipo de Dato Correcto");
 Console.WriteLine("-----------------------------------------\n");
+
+// Unidad 3: Deteccion de tipos de enteros
+
+Console.WriteLine("Signed integral types (enteros con signo):");
+Console.WriteLine("sbyte: " + sbyte.MinValue + " a " + sbyte.MaxValue);
+Console.WriteLine("short: " + short.MinValue + " a " + short.MaxValue);
+Console.WriteLine("int: " + int.MinValue + " a " + int.MaxValue);
+Console.WriteLine("long: " + long.MinValue + " a " + long.MaxValue);
+
+Console.WriteLine("\nUnsigned integral types (enteros sin signo):");
+Console.WriteLine("byte: " + byte.MinValue + " a " + byte.MaxValue);
+Console.WriteLine("ushort: " + ushort.MinValue + " a " + ushort.MaxValue);
+Console.WriteLine("uint: " + uint.MinValue + " a " + uint.MaxValue);
+Console.WriteLine("ulong: " + ulong.MinValue + " a " + ulong.MaxValue);
+
+// Unidad 4: Detección de tipos de punto flotante
 
 // -------------------------------------------------
 // Fin del programa
